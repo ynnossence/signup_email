c732344af11f7f7a1799d27c8e692678453d7c6b
Sonny Tambiac <ynnossence@gmail.com>
2012-10-22 23:52:02 +0800

    Added sidebar,fixed header and added footer.

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,28 @@
+$(function(){
+/*
+  var $panel = $('#sidebar-accordion .widget-content').hide();
+  var $allPanels = $('#sidebar-accordion .widget');
+  $('#sidebar-accordion .active').find('.widget-content').show();
+  $('#sidebar-accordion .widget-title').click(function() {
+      $this = $(this);
+      $target =  $this.parent();
+      if(!$target.hasClass('active')){
+         $allPanels.removeClass('active').find('.widget-content').slideUp();
+         $target.addClass('active').find('.widget-content').slideDown();
+      } 
+    return false;
+  });
+*/
+
+  $('#sidebar-accordion .active .widget-content').show();
+  $('#sidebar-accordion .widget-title').toggle(function(){
+  	$(this).next().slideDown(); 
+  	$(this).parent().addClass('active');
+  },function(){
+  	$(this).next().slideUp();  
+  	$(this).parent().removeClass('active');
+  })
+  
+
+
+});
